5477390f2f149058760e8f44f11889019b324ba3
NetAcad DEVASC <changeme@example.com>
2022-04-22 07:44:22 +0000

    versi 2

---
+++ mapquest_parse-json_2.py
@@ -0,0 +1,16 @@
+import urllib.parse
+import requests
+
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Rome, Italy"
+dest = "Frascati, Italy"
+key = "0PGpTlPtzDmf4AqdwA9C8IAmLGTblgHq"
+
+url = main_api + urllib.parse.urlencode({"key":key, "from":orig, "to":dest})
+
+print("URL: " + (url))
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
